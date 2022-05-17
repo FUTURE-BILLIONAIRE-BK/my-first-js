6849d7a9d2298396e6ba2d4995ae9af58d60b7cc
FUTURE-BILLIONAIRE-BK <bk_khasanov@mail.ru>
2022-05-17 18:33:37 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+alert('WELCOME TO OUR GAME')
+let num = prompt('Please enter the number and I can tell you that it is even or odd')
+num = Number(num)
+if (num%2==0)
+{
+  alert('Bro, it is even number, don\'t tell me that you don\'t know that ')
+}
+else if(num%2!==0)
+{
+  alert('Bro, it is odd number, you don\'t know it, ooh come on are you kidding?')
+}
